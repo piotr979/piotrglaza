7a14e3152ee32950ad0d70b3ba49bac43a63b008
Piotr Glaza <piotrglaza@gmail.com>
2024-02-06 20:56:47 +0100

    Front end

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'mono': ['Roboto mono', 'ui-monospace', 'SFMono-Regular'],
+    },
+    extend: {
+      colors: {
+        'primary-background': '#BCB5A4',
+        'secondary-background': '#646A5D',
+        'main-dark': '#333333',
+        'main-dark-tint-1': '#656565',
+        'main-light': "#CDCDCD",
+        'button-primary': '#4F4444',
+        'button-secondary': '#720D0D',
+        'button-ternary': '#0D4D72',
+      },
+    },
   },
   plugins: [],
 }
